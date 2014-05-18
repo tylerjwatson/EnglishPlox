ed644b84126e36df9c8737a1358d48235ed15bb8
Tyler Watson <tyler@tw.id.au>
2014-05-18 20:12:46 +1000

    Inital push of English, plox

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-EnglishPlox
+English, plox.
 ===========
 
-A simple plugin for TSAPI that kicks people that have characters in their name that aren't on a standard US-keyboard
+English, plox is a simple plugin for TSAPI that kicks people that have characters in their name 
+that aren't on a standard US-keyboard.  Terraria's console interface makes it extremely hard to 
+manage players that have characters that are easily typed on a non-US keyboard, so this plugin
+enforces people to have standard reduced-set ASCII characters in their character names only.
+
+List of allowed characters in names:
+
+* A-Z
+* 0-9
+* !@#$%^&*()_+-~`;:{}[]|.,
+
+List of denied characters in names:
+
+* Everything else
+
---
+++ Wolfje.Plugins.EnglishPlox/EnglishPloxPlugin.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Terraria;
+using TerrariaApi;
+
+namespace Wolfje.Plugins.EnglishPlox {
+    [TerrariaApi.Server.ApiVersion(1, 16)]
+    public class EnglishPloxPlugin : TerrariaApi.Server.TerrariaPlugin {
+        public static readonly Regex invalidCharactersRegex = new Regex(@"[^\da-z!@#\$%\^\&\*\(\)\-\+~ ;{}|\[\]:\.,_`]", RegexOptions.IgnoreCase);
+
+        public EnglishPloxPlugin(Terraria.Main game)
+            : base(game) {
+        }
+
+        public override string Author {
+            get {
+                return "Wolfje";
+            }
+        }
+
+        public override string Description {
+            get {
+                return "Forces everyone in the server to have a name consisting of only letters and numbers";
+            }
+        }
+
+        public override string Name {
+            get {
+                return "EnglishPlox";
+            }
+        }
+
+        public override Version Version {
+            get {
+                return Assembly.GetExecutingAssembly().GetName().Version;
+            }
+        }
+
+        public override void Initialize() {
+            TerrariaApi.Server.ServerApi.Hooks.ServerJoin.Register(this, Server_Join);
+        }
+
+        private void Server_Join(TerrariaApi.Server.JoinEventArgs args) {
+            Terraria.Player player = null;
+
+            if ((player = Terraria.Main.player.ElementAtOrDefault(args.Who)) == null) {
+                return;
+            }
+
+            if (invalidCharactersRegex.IsMatch(player.name) == true) {
+                StringBuilder sb = new StringBuilder();
+                foreach (Match m in invalidCharactersRegex.Matches(player.name)) {
+                    sb.Append(m.Value);
+                }
+                Terraria.NetMessage.SendData((int)PacketTypes.Disconnect, player.whoAmi, text: "Your name cannot contain these characters: " + sb.ToString());
+            }
+        }
+
+    }
+}
